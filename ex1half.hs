39de879c984e590a3439511a746b3d8adbf885aa
Dereck Piché <80223269+ScierKnave@users.noreply.github.com>
2022-09-16 15:33:16 -0400

    Add files via upload

---
+++ ex1half.hs
@@ -0,0 +1,29 @@
+--on veut faire quicksort avec Haskell
+quickSort :: [Int] -> [Int]
+quickSort list =
+    if length list <= 1
+        then list
+    else
+        let
+            pivot = head list --get first element of the list
+            -- get list of elements smaller than the pivot
+            smList = getLesserThan pivot list
+            -- get list of elements bigger than the pivot
+            biList = map negate ( getLesserThan (negate pivot) (map negate list) )
+        in 
+            (quickSort smList) ++ [pivot] ++ (quickSort biList)
+
+
+--returns list of all elements bigger or equal to a pivot in argument list
+getLesserThan :: Int -> [Int] -> [Int]
+getLesserThan pivot list =
+    if length list == 0
+        then list
+    else if (head list) < pivot
+        then [head list] ++ getLesserThan pivot (tail list)
+    else getLesserThan pivot (tail list)
+    
+
+
+
+
---
+++ ex1halfv3.hs
@@ -0,0 +1,50 @@
+
+--on veut faire quicksort avec Haskell
+quickSort :: [Int] -> [Int]
+quickSort list =
+    if length list <= 1
+        then list
+    else
+        let
+            left = tail ( fst (pivoting (head list) list) )
+            pivot = head ( fst (pivoting (head list) list) )
+            right = snd (pivoting (head list) list) 
+        in 
+            (quickSort left) ++ [pivot] ++ (quickSort right)
+
+
+
+--returns list of all elements bigger or equal to a pivot in argument list
+pivoting :: Int -> [Int] -> ([Int],[Int])
+pivoting pivot list =
+    if length list == 0
+        then ([],[])
+    --put list[p1] in smaller than or equal to pivot list and increment p1
+    else if head list <= pivot
+        then 
+            (
+                [head list] ++ fst (pivoting pivot (tail list))
+                ,
+                snd (pivoting pivot (tail list))
+            )
+    --put list[p2] in bigger than pivot list and decrement p2
+    else if last list > pivot
+        then 
+            (
+                fst (pivoting pivot (init list))
+                ,
+                snd (pivoting pivot (init list)) ++ [last list]
+            )
+    --put list[p1] in smaller than or equal to pivot list and increment p1
+    --put list[p2] in bigger than pivot list and decrement p2
+    else 
+            (
+                [last list] ++ fst (pivoting pivot (init (tail list)))
+                ,
+                snd (pivoting pivot (init (tail list))) ++ [head list] 
+            )
+
+
+
+
+

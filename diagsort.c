b1492289fb3742a824f8377d20c98f47f262a77b
Dereck Piché <pichedereck@gmail.com>
2022-09-28 18:11:35 -0400

    awd

--- diagsort.c
+++ diagsort.c
@@ -1,22 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*
-Description: Taking a matrix as input, return a copy were all the elements are sorted 
-on it's diagonal. (Problem #1329 of LeetCode)
+Description: Taking a matrix and sort all the elements on it's diagonals. 
+(Problem #1329 of LeetCode)
 Author: ScierKnave 
 Language: C
 */
 
-/*-----------------------------------------Utilities---------------------------------------- */
 
+void diagQuickSort(int** mat, int i, int j, int length)
+{
+    if (length < 2){
+        return;
+    }
+    int pivot = mat[i][j]; int temp;
+    int a = 0; int b = length-1;
 
-
-//return minimum of two integers
-int minimum(int a, int b){
-    if (a < b) { return a; }
-    return b;
+    while (a < b) {
+        while(a < length -1 && mat[i+a][j+a] <= pivot){
+            a++;
+        }
+        while(mat[i+b][j+b] > pivot){
+            b--;
+        }
+        if (a < b) {
+            temp = mat[i+a][j+a]; 
+            mat[i+a][j+a] = mat[i+b][j+b];
+            mat[i+b][j+b] = temp;
+        }
+    }
+    if (mat[i+a][j+a] > pivot){
+        a--;
+    }
+    temp = mat[i+a][j+a]; 
+    mat[i+a][j+a] = mat[i][j];
+    mat[i][j] = temp;
+    diagQuickSort(mat, i, j, a); 
+    diagQuickSort(mat, i+a+1, j+a+1, length-a-1);
 }
 
+
+
+
+//create a copy of mat were all the elements on it's diagonal are sorted
+//here, the arguments are generic and not necessary optimized for the problem
+int** diagonalSort(int** mat, int matSize, int* matColSize, int* returnSize, int** returnColumnSizes){
+    int m = matSize; int n =  matColSize[0]; 
+    int length;
+
+    *returnSize = m;
+    //set sizes of columns of returned matrix
+    *returnColumnSizes = malloc(sizeof(int)*m);
+    for (int i = 0; i < m; i++) (*returnColumnSizes)[i] = n;
+    
+    //call diagQuickSort on all the starting elements of the diagonals
+    for (int d = 0; d < m + n -1; d++){
+        if (d < m){
+            length = (m-d > n) ? n : m-d; //minimum
+            diagQuickSort(mat, d, 0, length);
+        }
+        else {
+            length = (m > n-(d-m+1)) ? n-(d-m+1) : m; //minimum
+            diagQuickSort(mat, 0, d-m+1, length);
+        }
+    }
+    return mat;
+}
+
+/*-----------------------------------------Test---------------------------------------- */
+
+//to allocate memory
 int** matrix(int m, int n){
     int **mat = (int **) malloc(m * sizeof(int*));
     for (int i = 0; i < m; i++){
@@ -34,72 +87,6 @@ void printMatrix(int** mat, int m, int n){
     }
 }
 
-/*-----------------------------------------Diagonal Sort---------------------------------------- */
-
-
-
-void Switch(int** mat, int i, int j, int i2, int j2){
-    int temp = mat[i][j];
-    mat[i][j] = mat[i2][j2];
-    mat[i2][j2] = temp;
-}
-
-
-//sorts the diagonal starting i and j
-void diagQuickSort(int** mat, int i, int j, int length)
-{
-    if (length < 2){
-        return;
-    }
-    int pivot = mat[i][j];
-    int a = 0; int b = length-1;
-
-    while (a < b) {
-        while(mat[i+a][j+a] <= pivot & a < b){
-            a++;
-        }
-        while(mat[i+b][j+b] > pivot & a < b){
-            b--;
-        }
-        Switch(mat, i+a, j+a, i+b, j+b);
-    }
-    Switch(mat, i, j, i+a, j+a);
-    diagQuickSort(mat, i, j, a); 
-    diagQuickSort(mat, i+a+1, j+a+1, length-b);
-}
-
-
-
-
-//create a copy of mat were all the elements on it's diagonal are sorted
-int** diagonalSort(int** mat, int matSize, int* matColSize, int* returnSize, int** returnColumnSizes){
-
-    //rename variables
-    int m = matSize; //number of lines
-    int n =  matColSize[0]; //number of columns
-    //set sizes of returned matrix
-    *returnSize = m;
-    *returnColumnSizes = malloc(sizeof(int)*m);
-    for (int i = 0; i < m; i++) (*returnColumnSizes)[i] = n;
-    
-    //sort all the diagonals
-    for (int d = 0; d < m + n -1; d++){
-        //if diag starts in first column
-        if (d < m){
-            int length = minimum(m-d, n);
-            diagQuickSort(mat, d, 0, length);
-        }
-        //if diag starts in first line AND not in first column
-        else {
-            int length = minimum(m, n-(d-m+1));
-            diagQuickSort(mat, 0, d-m+1, length);
-        }
-    }
-    return mat;
-}
-
-/*-----------------------------------------Main---------------------------------------- */
-
 int main(){
     //generate arguments
     int** mat = matrix(2,3);
@@ -107,7 +94,8 @@ int main(){
     mat[1][0] = 7; mat[1][1] = 2; mat[1][2] = 9;
     //printf("%d", mat[0][0]); 
     printMatrix(mat, 2, 3);
-    printf("\n");
+
+    printf("Sorted: \n");
     int matSize = 2;
     int matColSize[2] = {3,3};
     int rSize;
